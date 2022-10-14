a24f8691adc31bd317dc4da2233a446bede9777e
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-13 22:19:13 -0500

    se culmino con el ejercicio eje12.py

--- eje12.py
+++ eje12.py
@@ -1,5 +1,16 @@
+#Escribí un programa para solicitar al usuario el ingreso de un número entero
+#y que luego imprima un valor de verdad
+#dependiendo de si el número es par o no.
+#Recordar que un número es par si el resto, al dividirlo por 2, es 0.
+
 def run():
-    pass
+    numero=int(input("Ingrese un numero entero: "))
+    if numero%2==0:
+        print(True)
+    else:
+        print(False)
+
+        
 
 if __name__=='__main__':
     run()
