a3afa6864862a83d6a172316168e73526e170ed1
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-13 22:06:58 -0500

    ejercicio culminado

--- eje11.py
+++ eje11.py
@@ -3,16 +3,16 @@
 #los siguientes dos el mes y los últimos cuatro el año (DDMMAAAA).
 #Este dato debe guardarse en una variable con tipo int (número entero).
 #Finalmente, mostrar al usuario la fecha con el formato DD / MM / AAAA.
-def run ():
-    fecha=int(input("Ingrese una fecha con el siguiente formato DDMMAAAA: "))
+def run():
+    fecha=input("Ingrese una fecha con el siguiente formato DDMMAAAA: ")
     cadena=str(fecha)
-    dia=cadena[0,2]
-    mes=cadena[2,4]
-    año=cadena[4,9]
+    dia=cadena[0:2]
+    mes=cadena[2:4]
+    año=cadena[4:8]
     print(dia,"/",mes,"/",año)
 
 
 
-
+    
 if __name__=='__main__':
     run()
