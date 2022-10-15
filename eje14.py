fc0387182148599fb03ea96fab2c31e7c04ce536
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-15 11:56:45 -0500

    Se agrego linea de codigo de buena practica

--- eje14.py
+++ eje14.py
@@ -1,1 +1,6 @@
 #Escribí un programa que, dada una cadena de texto por el usuario, imprima True #si la cantidad de caracteres en la cadena es un número impar, o False si no lo #es.
+def run():
+    pass
+
+if __name__=='__main__':
+    run()
