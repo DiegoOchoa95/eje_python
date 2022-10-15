28d443d20a6a568d6a336bd81d9b4b15b09c4d18
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-15 12:02:16 -0500

    Se culmino con el ejercicio 14

--- eje14.py
+++ eje14.py
@@ -1,6 +1,15 @@
-#Escribí un programa que, dada una cadena de texto por el usuario, imprima True #si la cantidad de caracteres en la cadena es un número impar, o False si no lo #es.
+#Escribí un programa que, dada una cadena de texto por el usuario, 
+#imprima True si la cantidad de caracteres en la cadena es un número impar, 
+#o False si no lo es.
 def run():
-    pass
+    cadena=input("Ingrese una palabra por favor:")
+    cantidad=len(cadena)
+    if cantidad%2!=0:
+        print(True)
+    else:
+        print(False)
+
+
 
 if __name__=='__main__':
     run()
