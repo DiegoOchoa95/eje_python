912e7fcdafca8036515ccac816c16bd4e2bf9cba
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-17 16:20:27 -0500

    Se realizó una ultima prueba

--- eje18.py
+++ eje18.py
@@ -1,1 +1,14 @@
 #Escribí un programa que solicite al usuario el ingreso de dos números #diferentes y muestre en pantalla al mayor de los dos.
+def run():
+    n1=int(input("Ingrese el primer número entero: "))
+    n2=int(input("Ingrese el segundo número entero: "))
+    if n1>n2:
+        print("El número mayor ingresado es: ",n1)
+    else:
+        print("El número mayor ingresado es: ",n2)
+
+
+
+
+if __name__=='__main__':
+    run()
