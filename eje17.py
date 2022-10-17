9d15ed77366d861556c8b1d1d69648c4b5186c98
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-17 12:54:16 -0500

    Se culmino con el ejercicio 17

--- eje17.py
+++ eje17.py
@@ -1,7 +1,14 @@
 #Escribí un programa que, dado un número entero, muestre su valor absoluto. #Recordá que, para los números positivos su valor absoluto es igual al número
 #(el valor absoluto de 52 es 52), mientras que, para los negativos, su valor #absoluto es el número multiplicado por -1 (el valor absoluto de -52 es 52).
 def run():
-    pass
+    numero= int(input("Ingrese un número entero: "))
+    if numero>0:
+        print("El valor absoluto de ",numero," es: ",numero)
+    elif numero<0:
+        print("El valor absoluto de ",numero," es: ",(numero*-1))
+    else:
+        print("El numero ingresado es 0, por favor ingrese un numero mayor o menor a cero")
+
 
 
 if __name__=='__main__':
