8c9cddab74faa8ec21476948a6c299627016b97d
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-17 12:17:26 -0500

    Se culmino con el ejercicio numero 15

--- eje15.py
+++ eje15.py
@@ -1,6 +1,14 @@
 #Escribí un programa que le pida al usuario ingresar dos palabras y las guarde #en dos variables, y que luego imprima True si la primera palabra es menor que #la segunda o False si no lo es.
 def run():
-    pass
+    p1=input("Ingrese la primera palabra: ")
+    p2=input("Ingrese la segunda palabra: ")
+    tp1=len(p1)
+    tp2=len(p2)
+    if tp1>tp2:
+        print(True)
+    else:
+        print(False)
+        
 
 
 if __name__=='__main__':
