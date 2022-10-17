b562cc872fc972a7e10d6463f20db12a630f28c6
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-17 12:46:21 -0500

    Primer commit del archivo eje17.py, se agrego enunciado del problema

---
+++ eje17.py
@@ -0,0 +1,8 @@
+#Escribí un programa que, dado un número entero, muestre su valor absoluto. #Recordá que, para los números positivos su valor absoluto es igual al número
+#(el valor absoluto de 52 es 52), mientras que, para los negativos, su valor #absoluto es el número multiplicado por -1 (el valor absoluto de -52 es 52).
+def run():
+    pass
+
+
+if __name__=='__main__':
+    run()
