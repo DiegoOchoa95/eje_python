fc2436584a549e7ecde8483648fb0316ff72cc80
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-17 16:24:23 -0500

    Primer commit del ejercicio 19, se agrego enunciado del problema

---
+++ eje19.py
@@ -0,0 +1,8 @@
+#Escribí un programa que solicite al usuario una letra y, si es una vocal, #muestre el mensaje “Es vocal”. Verificar si el usuario ingresó un string de más #de un carácter y, en ese caso, informarle que no se puede procesar el dato.
+def run():
+    pass
+
+
+
+if __name__=='__main__':
+    run()
