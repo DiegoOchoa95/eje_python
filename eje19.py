de4e083cead74a7d5e8f5d9ff738e3060e268c68
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-17 18:52:08 -0500

    Se realizo una prueba final al ejercicio 19

--- eje19.py
+++ eje19.py
@@ -1,6 +1,15 @@
 #Escribí un programa que solicite al usuario una letra y, si es una vocal, #muestre el mensaje “Es vocal”. Verificar si el usuario ingresó un string de más #de un carácter y, en ese caso, informarle que no se puede procesar el dato.
 def run():
-    pass
+    letra=input("Ingrese una letra: ")
+    tamaño=len(letra)
+    if tamaño>=2:
+        print("Solo ingrese una letra")
+    else:
+        if letra=="a" or letra=="e" or letra=="i" or letra=="o" or letra=="u":
+             print("La letra ingresada es una vocal")
+        else:
+            print("La letra ingresada en una consonante")
+        
 
 
 
