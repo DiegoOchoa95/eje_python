4b658dbabee476e0746022cce08ba45049260bda
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-20 15:28:59 -0500

    Se culmino con la resolucion del ejercicio 21

--- eje21.py
+++ eje21.py
@@ -1,6 +1,12 @@
 #Escribí un programa que solicite ingresar un nombre de usuario y una #contraseña. Si el nombre es “Gwenevere” y la contraseña es “excalibur”, mostrar #en pantalla “Usuario y contraseña correctos. Puede ingresar a Camelot”. Si el #nombre o la contraseña no coinciden, mostrar “Acceso denegado”.
 def run():
-    pass
+    nusuario=input("Ingrese su usuario: ")
+    contraseña=input("Ingrese su contraseña: ")
+    if nusuario=="Gwenevere" and contraseña=="excalibur":
+        print("Usuario y Contraseña correctos. Puedes ingresar a Camelot")
+    else:
+        print("Acceso denegado")
+            
 
 
 if __name__=='__main__':
