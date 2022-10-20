a5de2b8aeaf46f1a7a80ece564a7705053fa77b1
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-20 15:38:04 -0500

    Primer commit del archivo eje22.py, se agrego enunciado del ejercicio

---
+++ eje22.py
@@ -0,0 +1,7 @@
+#Escribí un programa que permita saber si un año es bisiesto. Para que un año #sea bisiesto debe ser divisible por 4 y no debe ser divisible por 100, excepto #que también sea divisible por 400.
+def run():
+    pass
+
+
+if __name__=='__main__':
+    run()
