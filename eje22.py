41b711363e7bfb31f7086c27887b2bf10c428fd0
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-21 18:12:42 -0500

    Se culmino con el desarrollo del ejercicio 22

--- eje22.py
+++ eje22.py
@@ -1,6 +1,11 @@
 #Escribí un programa que permita saber si un año es bisiesto. Para que un año #sea bisiesto debe ser divisible por 4 y no debe ser divisible por 100, excepto #que también sea divisible por 400.
 def run():
-    pass
+    año=int(input("Ingrese un año: "))
+    if año%4==0 and año%100!=0:
+        print("El año ingresado es bisiesto")
+    else:
+        print("El año ingresado no es bisiesto")
+        
 
 
 if __name__=='__main__':
