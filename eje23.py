cd6a6112550db2d8ef1f43fd1e5da88ed2277e78
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-21 18:15:33 -0500

    Primer commit del archivo eje23.py, se agrego el enunciado del ejercicio

---
+++ eje23.py
@@ -0,0 +1,8 @@
+#Escribí un programa que le solicite al usuario un número entero y muestre todos #los números correlativos entre el 1 y el número ingresado por el usuario.
+def run():
+    pass
+
+
+if __name__=='__main__':
+    run()
+    
