cfb5aaa6f517e747a3f99171d60c8bba7d497124
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-24 10:52:25 -0500

    Se culmino con el ejercicio 24

--- eje24.py
+++ eje24.py
@@ -1,6 +1,9 @@
 #Escribí un programa que muestre la sumatoria de todos los números entre el 0 y el 100.
 def run():
-    pass
+    sumatoria=(100*(100+1))/2    
+    print(sumatoria)
+
+        
 
 if __name__=='__main__':
     run()
