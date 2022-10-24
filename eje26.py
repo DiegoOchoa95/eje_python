ed30236a3e3ab2af541a42cb4b42566884392736
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-24 11:51:26 -0500

    Se culmino con el desarrollo del ejercicio 26

---
+++ eje26.py
@@ -0,0 +1,16 @@
+#Escribí un programa que, dada una frase por el usuario, muestre la cantidad #total de vocales (tanto mayúsculas como minúsculas) que contiene.
+def run():
+    frase=input("Escribe una frase: ")
+    frase=frase.upper()
+    c=0
+    for i in range(len(frase)):
+        if frase[i]=="A" or frase[i]=="E" or frase[i]=="I" or frase[i]=="O" or frase[i]=="U" :
+            c+=1
+        
+
+    print("En la frase hay ", c, " vocales")
+
+
+
+if __name__=='__main__':
+    run()
