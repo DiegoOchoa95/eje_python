ebe615c883d5042ea9e1a32d82491d1fbab2ea78
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-24 15:43:00 -0500

    Se culmino con el desarrollo del ejercicio 27

--- eje27.py
+++ eje27.py
@@ -1,6 +1,17 @@
-#Escribí un programa que muestre los primeros 10 números de la sucesión de #Fibonacci. La sucesión comienza con los números 0 y 1 y, a partir de éstos, #cada elemento es la suma de los dos números anteriores en la secuencia: 0, 1, #1, 2, 3, 5, 8, 13, 21, 34, 55…
+#Escribí un programa que muestre los primeros 10 números de la sucesión de #Fibonacci. La sucesión comienza con los números 0 y 1 y, a partir de éstos, #cada elemento es la suma de los dos números anteriores en la secuencia: 
+#0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55…
 def run():
-    pass
+    n1=0
+    n2=1
+    print(n1)
+    print(n2)
+    for i in range(8):
+        n3=n1+n2
+        print(n3)
+        n1=n2
+        n2=n3
+        
+
 
 if __name__=='__main__':
     run()
