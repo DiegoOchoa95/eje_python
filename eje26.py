2d0e2df6b79791d510717330a0abe5015b448a35
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-24 12:36:10 -0500

    Se agrego codigo mas reducido al ejercicio 26

--- eje26.py
+++ eje26.py
@@ -1,4 +1,5 @@
 #Escribí un programa que, dada una frase por el usuario, muestre la cantidad #total de vocales (tanto mayúsculas como minúsculas) que contiene.
+
 def run():
     frase=input("Escribe una frase: ")
     frase=frase.upper()
@@ -14,3 +15,16 @@ def run():
 
 if __name__=='__main__':
     run()
+
+"""
+
+#Codigo extra
+frase=input("Frase:")
+vocales="aeiouAEIOU"
+cantidad=0
+for i in frase:
+    if i in vocales:
+        cantidad=cantidad+1
+print("Vocales:", cantidad)
+
+"""
