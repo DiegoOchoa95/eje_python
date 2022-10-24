0c4043a0ca7a18bf67df750d593337b7a93d1b69
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-24 15:46:39 -0500

    Primer commit del ejercicio 28, se agrego el enunciado del ejercicio

---
+++ eje28.py
@@ -0,0 +1,6 @@
+#Escribí un programa que, dado un número entero positivo, calcule y muestre su #factorial. El factorial de un número se obtiene multiplicando todos los números #enteros positivos que hay entre el 1 y ese número. El factorial de 0 es 1.
+def run():
+    pass
+
+if __name__=='__main__':
+    run()
