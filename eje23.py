0edd7c7fe146f5f3ff02a2cfb60b766b8733ebd4
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-24 10:17:35 -0500

    Se culmino con el ejercicio 23

--- eje23.py
+++ eje23.py
@@ -1,6 +1,11 @@
 #Escribí un programa que le solicite al usuario un número entero y muestre todos #los números correlativos entre el 1 y el número ingresado por el usuario.
 def run():
-    pass
+    numero=int(input("Ingrese un numero entero: "))
+    for i in range(numero):
+        i+=1
+        print(i)
+        
+       
 
 
 if __name__=='__main__':
