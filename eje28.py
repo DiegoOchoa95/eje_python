350fdc686a4fb499eebf81f5f2bd6af210aff09a
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-25 12:39:05 -0500

    Se culmino con el desarrollo del ejercicio 28

--- eje28.py
+++ eje28.py
@@ -1,6 +1,15 @@
 #Escribí un programa que, dado un número entero positivo, calcule y muestre su #factorial. El factorial de un número se obtiene multiplicando todos los números #enteros positivos que hay entre el 1 y ese número. El factorial de 0 es 1.
+#1,2,3,4,5
 def run():
-    pass
+    numero=int(input("Ingrese un numero para hallar su factorial: "))
+    n2=2
+    fact=1
+    for i in range(numero-1):
+        fact=fact*n2
+        print(fact)
+        n2+=1
+      
+
 
 if __name__=='__main__':
     run()
