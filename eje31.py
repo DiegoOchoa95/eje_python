56dd3bee0bd868ed2486d0d5f6fff773519b7269
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-25 17:56:55 -0500

    Primer commit del archivo eje31.py, se agrego enunciado del ejercicio

---
+++ eje31.py
@@ -0,0 +1,8 @@
+#Escribí un programa que, dada una frase por el usuario, la muestre invertida, #sin utilizar una rebanada con paso negativo.
+def run():
+    pass
+
+
+
+if __name__=='__main__':
+    run()
