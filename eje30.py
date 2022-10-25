7bd1b961793d1c1cab0e46787e0a3438651e626f
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-25 17:52:02 -0500

    Se culmino con el desarrollo del ejercicio 30

--- eje30.py
+++ eje30.py
@@ -1,6 +1,15 @@
 #Escribí un programa que permita al usuario ingresar una frase y luego un #carácter (string de longitud 1) y luego muestre la frase ingresada, pero con #todas las ocurrencias del carácter indicado por el usuario reemplazadas por “*”.
 def run():
-    pass
+    frase=input("Ingresar una frase: ")
+    caracter=input("Ingrese un caracter: ")
+    if len(caracter)>1:
+        print("Por favor ingrese un solo caracter")
+        caracter=input("Ingrese un caracter: ")
+    
+    caracter=caracter.lower()
+    frase=frase.lower()
+    frase=frase.replace(caracter,"*")
+    print(frase)
 
 
 if __name__=='__main__':
