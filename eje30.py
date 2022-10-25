86cd00f1549694701216378eace5241d7a58aaaf
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-25 17:38:26 -0500

    Primer commit del ejercicio 30, se agrego enunciado del problema

---
+++ eje30.py
@@ -0,0 +1,7 @@
+#Escribí un programa que permita al usuario ingresar una frase y luego un #carácter (string de longitud 1) y luego muestre la frase ingresada, pero con #todas las ocurrencias del carácter indicado por el usuario reemplazadas por “*”.
+def run():
+    pass
+
+
+if __name__=='__main__':
+    run()
