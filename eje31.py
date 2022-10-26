995db26cab26781b2f9374137775d00397a7c8c0
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-26 08:49:25 -0500

    Se culmino con el desarrollo del ejercicio 31,desarrollado de dos maneras

--- eje31.py
+++ eje31.py
@@ -1,6 +1,18 @@
 #Escribí un programa que, dada una frase por el usuario, la muestre invertida, #sin utilizar una rebanada con paso negativo.
 def run():
-    pass
+    #Primera forma
+    frase=input("Ingrese una frase: ")
+    frase=frase[::-1]
+    print(frase)
+    
+    #Segunda forma
+    frase=input("Frase:")
+    nueva=""
+    i=len(frase)-1
+    while i>=0:
+        nueva=nueva+frase[i]
+        i=i-1
+    print(nueva)
 
 
 
