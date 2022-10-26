84e7c2f3da0780b0032d63787c59ffdcb10d1ec2
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-26 12:10:56 -0500

    Primer commit del ejercicio 33, se agrego enunciado del problema

---
+++ eje33.py
@@ -0,0 +1,7 @@
+#Escribí un programa que permita al usuario ingresar una cantidad de números #positivos indefinida (la cantidad que ingresará no se conoce y puede cambiar en #cada ejecución), finalizando cuando ingresa el número 0 (que no se tendrá en #cuenta). Una vez terminada la lectura de números, informar cuál fue el mayor de #los números ingresados.
+def run():
+    pass
+
+
+if __name__=='__main__':
+    run()
