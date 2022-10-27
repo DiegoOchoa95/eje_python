4a56563363e678a592d28149b7ee78dbb7440a6c
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-10-27 12:44:10 -0500

    Primer commit del ejercicio 35, se agrego enunciado del problema

---
+++ eje35.py
@@ -0,0 +1,7 @@
+#Escribí un programa que solicite al usuario el ingreso de strings de longitud 1 (un solo carácter), uno por vez. La repetición finalizará cuando se ingrese un string que no tenga longitud 1, o cuando el string ingresado corresponda al dígito numérico 0. Al finalizar, mostrar el string completo que se formó con todos los caracteres ingresados y qué porcentaje de caracteres del total fueron la letra “a”.
+def run():
+    pass
+
+
+if __name__=='__main__':
+    run()
