55460858504046eac9f3814a08ff026125c9ab51
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-11-02 18:39:00 -0500

    Primer commit del ejercicio 38, se agrego el enunciado del ejercicio 38

---
+++ eje38.py
@@ -0,0 +1,6 @@
+#Escribí un programa que solicite al usuario una cadena de caracteres (que puede contener letras, números o símbolos). Analizar la cadena para mostrar: cuántas letras del abecedario (minúsculas y mayúsculas) contiene, cuántos símbolos (caracteres que no son ni letras ni números), cuántos dígitos numéricos y, de los dígitos, cuántos son múltiplos de 4.
+def run():
+    pass
+
+if __name__=='__main__':
+    run()
