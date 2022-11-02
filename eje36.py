3c8506607998345d8ce07f934bc650905d585951
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-11-02 10:06:11 -0500

    Primer commit del ejercicio 36, se agrego el enunciado del problema

---
+++ eje36.py
@@ -0,0 +1,7 @@
+#Escribí un programa que, dado un número entero por el usuario (guardado como int), muestre la suma de todos sus dígitos. Recordá que vas a necesitar obtener cada uno de los dígitos por separado para poder sumarlos entre sí.
+def run():
+    pass
+
+
+if __name__=='__main__':
+    run()
