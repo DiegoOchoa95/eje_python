f656365c10f7868337e18a5facd1017332180394
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-11-02 11:24:30 -0500

    Primer commit del ejercicio 37, se agrego enunciado del ejercicio

---
+++ eje37.py
@@ -0,0 +1,7 @@
+#Escribí un programa para solicitar al usuario que ingrese números enteros positivos (la cantidad que ingresará no se conoce y la decide el usuario). La lectura de números finalizará cuando el usuario ingrese el número -1. 
+#Por cada número ingresado, mostrar la cantidad de dígitos pares y la cantidad de dígitos impares que tiene. Al finalizar, mostrar cuántos números múltiplos de 3 ingresó el usuario.
+def run():
+    pass
+
+if __name__=='__main__':
+    run()
