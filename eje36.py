524006fb230830fa23e8807ef96b95a19a188656
Diego Eduardo Ochoa Orellana <diegoochoaorellana@gmail.com>
2022-11-02 11:19:07 -0500

    Se culmino con el desarrollo del ejercicio 36

--- eje36.py
+++ eje36.py
@@ -1,6 +1,12 @@
 #Escribí un programa que, dado un número entero por el usuario (guardado como int), muestre la suma de todos sus dígitos. Recordá que vas a necesitar obtener cada uno de los dígitos por separado para poder sumarlos entre sí.
 def run():
-    pass
+    numero=int(input("Ingresa un número:"))
+    total=0
+    while numero != 0:
+        ultimoDigito=numero%10
+        total=total+ultimoDigito
+        numero=numero//10
+    print("Suma de los dígitos:", total)
 
 
 if __name__=='__main__':
